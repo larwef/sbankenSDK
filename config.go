0c82fd75a271a06dd017cf38c71d69c2474853f8
Lars Wefald <lars.wefald@embriq.no>
2018-01-27 17:58:46 +0100

    Added function for loading config from json

--- config.go
+++ config.go
@@ -1,9 +1,30 @@
 package sbankenSDK
 
+import (
+	"io/ioutil"
+	"fmt"
+	"os"
+	"encoding/json"
+)
+
 type Config struct {
-	ClientId             string
-	ClientSecret         string
-	IdentityServer       string
-	AccountsEndpoint     string
-	TransactionsEndpoint string
+	ClientId             string `json:"clientId"`
+	ClientSecret         string `json:"clientSecret"`
+	IdentityServer       string `json:"identityServer"`
+	AccountsEndpoint     string `json:"accountsEndpoint"`
+	TransactionsEndpoint string `json:"transactionsEndpoint"`
 }
+
+func ConfigFromFile(filepath string) (Config) {
+	file, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		fmt.Printf("File error: %v\n", err)
+		os.Exit(1)
+	}
+
+	var config Config
+	json.Unmarshal(file, &config)
+
+	return config
+}
+
